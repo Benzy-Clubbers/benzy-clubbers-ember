f8bcfd624902a76a537a6a2a0bcf63d0196a725e
Tony <tonynguyen0925@gmail.com>
2021-05-06 22:00:10 -0700

    Added Carousel with no Functionality

--- tests/integration/components/carousel-test.js
+++ tests/integration/components/carousel-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | benzy-navbar', function(hooks) {
+module('Integration | Component | carousel', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<BenzyNavbar />`);
+    await render(hbs`<Carousel />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <BenzyNavbar>
+      <Carousel>
         template block text
-      </BenzyNavbar>
+      </Carousel>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
